bfbc72871a95f8bdef7ef047c01cd435c657852f
amr ramadan <amrramadan1@outlook.com>
2018-10-07 00:49:00 +0200

    database test_1

--- STF_LSTP/STF_LSTP/HELLOWORLD.py
+++ STF_LSTP/STF_LSTP/HELLOWORLD.py
@@ -1,1 +1,7 @@
-print("tuna")
+
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+from django.db import connection
+cursor = connection.cursor()
+cursor.execute("INSERT INTO Players (playerID, SID, firstName, lastName, email, phoneNumber, birthDate) VALUES (5, 5, 'karker', 'tuna', 'email', 123, '2018-11-07')")
+connection.commit
